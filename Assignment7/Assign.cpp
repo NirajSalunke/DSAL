56381fabf12d17b1a57724454086ef95ab6e9583
Niraj Salunke <nirajsalunke07@gmail.com>
2025-05-07 21:28:17 +0530

    Update Assign.cpp

--- Assignment7/Assign.cpp
+++ Assignment7/Assign.cpp
@@ -1,168 +1,150 @@
-/*
-
-You have a business with several offices; you want to lease phone lines
-to connect them up with each other; and the phone company charges
-different amounts of money to connect different pairs of cities. You
-want a set of lines that connects all your offices with a minimum total
-cost. Solve the problem by suggesting appropriate data structures.
-*/
-
 #include <iostream>
+#include <vector>
+#include <queue>
 #include <map>
-
 using namespace std;
-const int max = INT_MAX;
 
 class Graph
 {
-
-    int **adjMatix;
-    int vertexCount;
-    int edgeCount;
+    vector<vector<long>> adjMat;
     map<string, int> cities;
     map<int, string> inv_cities;
+    map<string, bool> visited;
+    int vertexCount;
+    int edgeCount;
+    using CityDistance = pair<pair<string, string>, int>;
 
-    void add(string city1, string city2, int dist)
+    void addDistBetnCities(string City1, string City2, long Distance)
     {
-        adjMatix[cities[city1]][cities[city2]] = dist;
-        adjMatix[cities[city2]][cities[city1]] = dist;
+        adjMat[cities[City1]][cities[City2]] = Distance;
+        adjMat[cities[City2]][cities[City1]] = Distance;
     }
 
-    // minimum_key() method for finding the vertex that has minimum key-value and that is not added in MST yet
-    int minimum_key(int key[], bool mst[])
+    // prim
+    pair<vector<CityDistance>, long> getMinRoadToAll()
     {
-        int minimum = INT_MAX, min, i;
+        long minDist = 0;
+        visited.clear();
 
-        // iterate over all vertices to find the vertex with minimum key-value
-        for (i = 0; i < vertexCount; i++)
-            if (mst[i] == 0 && key[i] < minimum)
+        auto compare = [](const CityDistance &a, const CityDistance &b)
+        {
+            return a.second > b.second;
+        };
+
+        priority_queue<CityDistance, vector<CityDistance>, decltype(compare)> pq(compare);
+        vector<CityDistance> MST;
+        pq.push({{inv_cities[0], inv_cities[0]}, -1});
+
+        while (!pq.empty())
+        {
+            auto temp = pq.top();
+            pq.pop();
+            if (visited[temp.first.second])
+                continue;
+
+            visited[temp.first.second] = true;
+            if (temp.second != -1)
             {
-                minimum = key[i];
-                min = i;
+                MST.push_back(temp);
+                minDist += temp.second;
             }
-        return min;
+            for (int i = 0; i < vertexCount; i++)
+            {
+                if (adjMat[cities[temp.first.second]][i] != 0 && !visited[inv_cities[i]])
+                {
+                    pq.push({{temp.first.second, inv_cities[i]}, adjMat[cities[temp.first.second]][i]});
+                }
+            }
+        }
+        return {MST, minDist};
     }
 
 public:
     Graph()
+
     {
-        cout << "Enter number of Vertices: ";
+        cout << "Enter Total Number of cities:- ";
         cin >> vertexCount;
-        cout << "Enter number of edges: ";
+        cout << "Enter Total Number distance betn citites , we know:- (edges) ";
         cin >> edgeCount;
-
-        adjMatix = new int *[vertexCount];
         for (int i = 0; i < vertexCount; i++)
         {
-            adjMatix[i] = new int[vertexCount];
-            for (int j = 0; j < vertexCount; j++)
-                adjMatix[i][j] = 0;
+            vector<long> arr(vertexCount, 0);
+            adjMat.push_back(arr);
         }
-
-        cout << "Add cities in order: " << endl;
         for (int i = 0; i < vertexCount; i++)
         {
-            string city;
-            cin >> city;
-            cities.insert(pair<string, int>(city, i));
-            inv_cities.insert(pair<int, string>(i, city));
+            string name;
+            cout << "Enter City " << i + 1 << " name:- ";
+            cin >> name;
+            cities[name] = i;
+            inv_cities[i] = name;
         }
-
-        //        for(auto i: cities) {
-        //            cout << i.first << " \t\t " << i.second << endl;
-        //        }
-
-        cout << "Add the " << edgeCount << " edges" << endl;
-
         for (int i = 0; i < edgeCount; i++)
         {
-            string u, v;
-            int d;
-            cout << "City 1: ";
-            cin >> u;
-            cout << "City 2: ";
-            cin >> v;
-            cout << "Distance: ";
-            cin >> d;
-            add(u, v, d);
+            cout << "Edge number " << i + 1 << endl;
+            add();
         }
-
-        //        for(int i=0; i<vertexCount; i++) {
-        //            for(int j=0; j<vertexCount; j++) {
-        //                cout<<adjMatix[i][j]<<" ";
-        //            }
-        //            cout<<endl;
-        //        }
     }
-
-    void prim(Graph g)
+    void printer()
     {
-        int n = g.vertexCount;
-
-        // create parent[] array of size equal to total number of vertices for storing the MST
-        // purpose of the array is to store parent vertex of successive vertices so as to form a tree
-        int parent[n];
-
-        // create key[] array for selecting an edge having minimum weight
-        int key[n];
-
-        // create mst[] array for seeing if a vertex has been added to MST or not
-        bool mst[n];
-
-        // initialise key values to some maximum value
-        // initialise mst values to 0 to indicate no vertex has been added to mst initially
-        for (int i = 0; i < n; i++)
+        cout << "  ";
+        for (int k = 0; k < vertexCount; k++)
         {
-            key[i] = INT_MAX;
-            mst[i] = 0;
+            cout << inv_cities[k] << " ";
         }
-
-        // we select the first vertex as the starting point for the algorithm
-        // edge weight of first vertex only = 0
-        key[0] = 0;
-        // since first vertex itself is the parent, it doesn't have a parent, so we set a garbage value
-        parent[0] = -1;
-
-        for (int count = 0; count < n - 1; count++)
+        cout << endl;
+        for (int i = 0; i < vertexCount; i++)
         {
-            // select the vertex having minimum key and that is not added in the MST yet from the set of vertices
-            // and set its flag to 1 to indicate that it has been added to MST
-            int min_edge_vertex = minimum_key(key, mst);
-            mst[min_edge_vertex] = 1;
-
-            for (int vertex = 0; vertex < n; vertex++)
+            for (int j = 0; j < vertexCount; j++)
             {
-                /*
-                 Now we find all adjacent vertices from the current (min_egde_vertex) vertex
-                 and perform 3 condition check to update the neighbour vertex's parent and distance:
-                    - Edge exists between the vertices
-                    - Neighbouring vertex has not been already visited
-                    - Distance / weight from the current vertex is lesser than distance from any previous vertex
-                 */
-                if (g.adjMatix[min_edge_vertex][vertex] && mst[vertex] == 0 && g.adjMatix[min_edge_vertex][vertex] < key[vertex])
+                if (j == 0)
                 {
-                    // update parent and weights of neighbouring vertices which satify the conditions above
-                    parent[vertex] = min_edge_vertex;
-                    key[vertex] = g.adjMatix[min_edge_vertex][vertex];
+                    cout << inv_cities[i] << " ";
                 }
+                cout << adjMat[i][j] << " ";
             }
+            cout << endl;
         }
+    }
+    void add()
+    {
+        string name1, name2;
+        long dist;
+        cout << "Enter City1 name:- ";
+        cin >> name1;
+        cout << "Enter City2  name:- ";
+        cin >> name2;
+        cout << "Enter Distance  betn:- ";
+        cin >> dist;
+        addDistBetnCities(name1, name2, dist);
+    }
+    void printBestPath()
+    {
+        pair<vector<CityDistance>, long> result = getMinRoadToAll();
+        cout << "Min Distance covering to travel the all cities:- " << result.second << endl;
 
-        cout << "MST of given graph is: \n"
-             << endl;
-        cout << "\nEdge \t Weight\n";
-        for (int i = 1; i < n; i++)
+        for (auto it : result.first)
         {
-            cout << inv_cities[parent[i]] << " <-> " << inv_cities[i] << " \t " << g.adjMatix[i][parent[i]] << endl;
+            cout << it.first.first << " -> " << it.first.second << " distance betn them => " << it.second << endl;
         }
     }
 };
 
 int main()
 {
-
     Graph g;
-    g.prim(g);
-
+    g.printer();
+    string yes;
+    cout << "Wanna visit all citites with least petrol expenses (ahh ik u got less mileage) :- enter yes or no?" << endl;
+    cin >> yes;
+    if (yes == "yes" || yes == "Yes" || yes == "YES")
+    {
+        g.printBestPath();
+    }
+    else
+    {
+        cout << "Looks like u have good mileage" << endl;
+    }
     return 0;
 }
